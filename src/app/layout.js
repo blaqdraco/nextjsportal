fc4a0662c2e433043a60476343ad8916201b87ec
IMANI LAMECK  KIRENGA <36161951+blaqdraco@users.noreply.github.com>
2025-09-25 22:01:39 +0300

    layout js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import LanguageProvider from './providers/LanguageProvider'
+import NetworkBackground from './components/NetworkBackground'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -46,6 +47,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NetworkBackground />
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
